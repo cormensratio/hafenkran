fda7ec9ec68af03c90712e036acb6bcb693fb5da
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-19 11:31:34 +0100

    EPDS02-92 update build for client

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ git submodule update --recursive --remote
 mvn clean package  -DskipTests -f hafenkran-clusterservice
 mvn clean package  -DskipTests -f hafenkran-userservice
 mvn clean package  -DskipTests -f hafenkran-reportingservice
+
+cd ./hafenkran-client
+npm install
+npm start
+
+cd ..
