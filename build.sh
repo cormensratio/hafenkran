d39d1098088992ba1cad9c6bc6e9a60b7770fc97
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-19 14:25:00 +0100

    EPDS02-92 update compose script

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-git submodule update --recursive --remote
+git submodule foreach "(git checkout develop; git pull; cd ..)"
 mvn clean package  -DskipTests -f hafenkran-clusterservice
 mvn clean package  -DskipTests -f hafenkran-userservice
 mvn clean package  -DskipTests -f hafenkran-reportingservice
 
 cd ./hafenkran-client
 npm install
-npm start
-
 cd ..
--- setVersion.sh
+++ setVersion.sh
@@ -15,3 +15,6 @@ function setMavenVersion {
 setMavenVersion hafenkran-clusterservice
 setMavenVersion hafenkran-userservice
 setMavenVersion hafenkran-reportingservice
+
+cd ./hafenkran-client
+npm version --no-git-tag-version $version
