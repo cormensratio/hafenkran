77b2059efd00be1ae3f45e522a76bdc340e8225b
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-22 10:30:52 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-git submodule foreach "(git checkout develop; git pull; cd ..)"
+git submodule init
+git submodule update
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
+
 mvn clean package  -DskipTests -f hafenkran-clusterservice
 mvn clean package  -DskipTests -f hafenkran-userservice
 mvn clean package  -DskipTests -f hafenkran-reportingservice
-
-cd ./hafenkran-client
-npm install
-cd ..
