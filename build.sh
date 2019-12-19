51bea3f9c446992cb4966d8330ac55f8d46a54ff
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-12-19 11:01:08 +0100

    update submodules

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
-
 mvn clean package  -DskipTests -f hafenkran-clusterservice
 mvn clean package  -DskipTests -f hafenkran-userservice
 mvn clean package  -DskipTests -f hafenkran-reportingservice
